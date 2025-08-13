c3bbeb5ada1aa7363a8adb51b79ae4c0730372ce
FleetFlow <73992175+Jones-Bros-Civil-Engineering@users.noreply.github.com>
2025-08-13 17:23:09 +0100

    Add plant coordinator page for asset allocation

--- FleetFlow/src/App.tsx
+++ FleetFlow/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CalendarPage from './pages/CalendarPage'
+import PlantCoordinatorPage from './pages/PlantCoordinatorPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CalendarPage />} />
+        <Route path="/plant-coordinator" element={<PlantCoordinatorPage />} />
       </Routes>
     </BrowserRouter>
   )
